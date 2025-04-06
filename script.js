c6c7b6e1d5052ff8a36a5b2b2cf71e867e974396
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 17:02:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let para=document.getElementById("counter");
 //your JS code here. If required.
 let count=0;
 function mouse(){
+	alert(count)
 	count++;
 	para.innerHTML=count; 
-	alert(count)
+
 }
 let myBtn=document.getElementById("incrementBtn");
 myBtn.addEventListener("click",(e)=>{
