8e7cec7246ff17d3b3bd3ada89c8070c10fc2e25
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 12:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mouse(){
 	let count=0;
 	count++;
 }
-alert()
+alert(localhost:3000 says)
 let myBtn=document.getElementById("Btn");
 myBtn.addEventListener("click",(e)=>{
 	console.log("clicked!!",e);
