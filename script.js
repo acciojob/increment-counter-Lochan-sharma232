3130a0d825e9a3f06d3c68e503c3f5cac3416434
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 11:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your JS code here. If required.
+function mouse(){
+	let count=0;
+	count++;
+}
 let myBtn=document.getElementById("Btn");
 myBtn.addEventListener("click",(e)=>{
 	console.log("clicked!!",e);
